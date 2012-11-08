2c0d1886a3bf43d51e8ec31e1ce0a95576063d14
Oskar Eriksson <oskar.det@gmail.com>
2012-11-08 18:24:03 +0100

    Overdrive defaults

--- example/js/main.js
+++ example/js/main.js
@@ -6,7 +6,7 @@ webkitAudioContext &&
         tuna = new Tuna(context), 
         player = document.getElementById('player'),
         sourceNode = context.createMediaElementSource(player),
-        names = ["Compressor","Filter","Cabinet","Chorus","Convolver","Delay","Overdrive","WahWah", "Tremolo", "Phaser"],
+        names = ["Overdrive","Compressor","Filter","Cabinet","Chorus","Convolver","Delay","WahWah", "Tremolo", "Phaser"],
         proto = "prototype",
         tabs = Object.create(null),
         effects = Object.create(null),
