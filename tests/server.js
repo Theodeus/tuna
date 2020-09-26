1830482575cfd580078eb28786e7497b92a5cd9f
theodeus <oskar.det@gmail.com>
2020-09-26 10:49:43 +0200

    Made sure server.js is included (ie. removed from gitignore)

---
+++ tests/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '..')));
+app.listen(8000, () => console.log('Example app listening on port 8000!'));
